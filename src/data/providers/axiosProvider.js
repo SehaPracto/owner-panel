82415b0b452a8df7398a4a7bf1e908f1793816ec
UNIGPATECH <unigpa.tech@gmail.com>
2022-05-10 18:00:48 +0300

    [MOD.] api connection

--- src/data/providers/axiosProvider.js
+++ src/data/providers/axiosProvider.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 class AxiosProvider {
+  //add url to work with
   // baseUrl = "http://192.168.1.217:3000/adminapi";
   baseUrl = "https://app.sehapracto.com/adminapi";
   // baseUrl = "http://10.106.13.212:3000/adminapi";

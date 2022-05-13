fde457555369ed1023040c8cd86e29519b8d6bc2
UNIGPATECH <unigpa.tech@gmail.com>
2022-05-13 15:04:03 +0300

    [MOD.] password fields

--- src/modules/admin_profile/components/AdminProfileChangePasswordTab.vue
+++ src/modules/admin_profile/components/AdminProfileChangePasswordTab.vue
@@ -3,21 +3,21 @@
     <form class="change-password-con" @submit.prevent="changePassword">
       <input
         class="input-text"
-        type="text"
+        type="password"
         placeholder="Old password"
         required
         v-model="oldPassword"
       />
       <input
         class="input-text"
-        type="text"
+        type="password"
         placeholder="New Password"
         required
         v-model="newPassword"
       />
       <input
         class="input-text"
-        type="text"
+        type="password"
         placeholder="Confirm New Password"
         required
         v-model="confirmNewPassword"
